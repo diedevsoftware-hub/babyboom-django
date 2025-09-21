0c5021003372c1fb58e155543b39f4d09d2c0413
Anderson Dietrich Garcia Chilon <dietrichandersongarciachilon@gmail.com>
2025-09-20 19:23:18 -0500

    Final fix for serving media files on Render

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static # Importante
+from django.conf.urls.static import static # Importante para los archivos de medios
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
 
@@ -16,12 +16,15 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('', include('core.urls')),
     path('cart/', include('cart.urls')),
     path('', include('products.urls')),
+    # --- IMPORTANTE: La URL de core debe ir al final ---
+    # Para que las rutas de productos como '/bicicletas/' no sean interceptadas primero.
+    path('', include('core.urls')),
 ]
 
-# --- CORRECCIÓN CRÍTICA ---
-# Esta línea SOLO se ejecutará si DEBUG es True (en desarrollo)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# --- CORRECCIÓN FINAL PARA IMÁGENES EN RENDER ---
+# Esta línea ahora se ejecuta siempre.
+# En desarrollo, el servidor de Django la usa.
+# En producción (Render), WhiteNoise la intercepta y sirve los archivos de forma segura.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
